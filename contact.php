01a86cdee83356d1773c1bc4751c8f4e28befa5a
SwiftDesignsStudio <info@swiftdesignsstudio.com>
2026-02-03 17:02:47 -0800

    Updated contact.php

--- contact.php
+++ contact.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
             $mail->Username   = 'info@swiftdesignsstudio.com';
-            $mail->Password   = 'kedt pvhk plig whls';
+            $mail->Password   = 'kedtpvhkpligwhls';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
